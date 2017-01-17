6870f18b859c6e823d29ffbad83942876f4def4d
vogtn <nicolasjvogt@gmail.com>
2017-01-16 20:45:52 -0800

    basic mongodb posts

--- server.js
+++ server.js
@@ -4,13 +4,15 @@ var bodyParser = require('body-parser')
 var app = express()
 app.use(bodyParser.json())
 
-app.get('/api/posts', function (req, res) {
-  res.json([
-    {
-      username: 'dickeyxxx',
-      body: 'node rocks!'
-    }
-  ])
+app.get('/api/posts', function (req, res, next) {
+  var post = new Post({
+    username: req.body.username,
+    body: req.body.body
+  })
+  post.save(function (err, post){
+    if(err) {return next(err)}
+    res.json(201, post)
+  })
 })
 
 app.post('/api/posts', function(req, res) {
