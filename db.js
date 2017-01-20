2111f91f76ac15e0a7f0d13f3b7df727d5f9d0eb
vogtn <nicolasjvogt@gmail.com>
2017-01-19 18:32:38 -0800

    fixes

--- db.js
+++ db.js
@@ -3,11 +3,3 @@ mongoose.connect('mongodb://localhost/social', function() {
   console.log('mongodb connected')
 })
 module.exports = mongoose
-
-var db = require('../db')
-var Post = db.model('Post', {
-  username: { type: String, required: true },
-  body: { type: String, required: true },
-  date: { type: String, require: true, default: Date.now }
-})
-module.exports = Post
--- server.js
+++ server.js
@@ -1,28 +1,30 @@
-var express = require('express')
-var post = require('posts')
+var express    = require('express')
 var bodyParser = require('body-parser')
+var Post       = require('./models/post')
+var logger     = require('morgan')
 
 var app = express()
 app.use(bodyParser.json())
+app.use(logger('dev'))
 
 app.get('/api/posts', function (req, res, next) {
-  Post.find(function(err, posts) {
-    if(err) {return next(err)}
+  Post.find(function (err, posts) {
+    if (err) { return next(err) }
     res.json(posts)
   })
 })
 
-app.post('/api/posts', function(req, res) {
+app.post('/api/posts', function (req, res, next) {
   var post = new Post({
     username: req.body.username,
-    body: req.body.body
+    body:     req.body.body,
   })
-  post.save(function (err, post){
-    if(err) {return next(err)}
-    res.json(201, post)
+  post.save(function (err, post) {
+    if (err) { return next(err) }
+    res.status(201).json(post)
   })
 })
 
-app.listen(3000, function() {
-  console.log('Server listening on', 3000)
+app.listen(3000, function () {
+  console.log('server listening on', 3000)
 })
